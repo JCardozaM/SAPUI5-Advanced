a745fa5e3bb49ab0fea243bb266cd0b43d872f1e
JCardozaM <jcardozam@icloud.com>
2022-11-10 19:18:16 +0000

    First Commit to 03_Proyecto_Fiori

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["esj/employees/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"esj/employees/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
